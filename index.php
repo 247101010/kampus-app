eb81f738e1fa248dcfc12e0c08b4edee345afbb9
ARIA MUHAMMAD FAHLEVI <ariamuhammadfahlevi23@gmail.com@gmail.com>
2026-02-25 13:32:24 +0700

    menambahkan design

--- index.php
+++ index.php
@@ -9,38 +9,41 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <head>
     <title>Data Mahasiswa</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-    <h2>Daftar Mahasiswa</h2>
-    <a href="tambah.php">Tambah Data</a>
-    <br><br>
+    <div class="container">
+        <h2>Daftar Mahasiswa</h2>
+        <a href="tambah.php" class="add-link">Tambah Data</a>
+        <br><br>
 
-    <table border="1" cellpadding="10">
-        <tr>
-            <th>No</th>
-            <th>NPM</th>
-            <th>Nama</th>
-            <th>Jurusan</th>
-            <th>Aksi</th>
-        </tr>
-
-        <?php $no = 1;
-        foreach ($data as $row): ?>
+        <table>
             <tr>
-                <td><?= $no++ ?></td>
-                <td><?= htmlspecialchars($row['npm']) ?></td>
-                <td><?= htmlspecialchars($row['nama']) ?></td>
-                <td><?= htmlspecialchars($row['jurusan']) ?></td>
-                <td>
-                    <a href="edit.php?id=<?= $row['id'] ?>">Edit</a>
-                    <a href="hapus.php?id=<?= $row['id'] ?>">Hapus</a>
-                </td>
+                <th>No</th>
+                <th>NPM</th>
+                <th>Nama</th>
+                <th>Jurusan</th>
+                <th>Aksi</th>
             </tr>
-        <?php endforeach; ?>
 
-    </table>
+            <?php $no = 1;
+            foreach ($data as $row): ?>
+                <tr>
+                    <td><?= $no++ ?></td>
+                    <td><?= htmlspecialchars($row['npm']) ?></td>
+                    <td><?= htmlspecialchars($row['nama']) ?></td>
+                    <td><?= htmlspecialchars($row['jurusan']) ?></td>
+                    <td>
+                        <a href="edit.php?id=<?= $row['id'] ?>">Edit</a>
+                        <a href="hapus.php?id=<?= $row['id'] ?>">Hapus</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+
+        </table>
+    </div>
 
 </body>
 
